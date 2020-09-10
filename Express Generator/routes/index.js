402d1928506c4b55674b82004729a3c6d59a9b6f
Avvaru Arjun <kingarjun99@gmail.com>
2020-09-10 13:54:46 +0530

    Upload

---
+++ Express Generator/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let data=[{
+    name:"Kiran",
+    age:30,
+    salary: 40000
+  },
+  {
+    name: "Ravi",
+    age: 40,
+    salary: 50000,
+  },
+  ]
+
+  res.render('index', { title: 'Express', MyName: 'Labbes Khan'});
+});
+
+module.exports = router;
---
+++ flipkart/src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Footer =()=>{
+    return <div><h1>I am Spiderman</h1></div>
+};
+
+export default Footer;
